b4ccf6f2af7047e81385deee83a89533f7c0b9d1
Petri Järvisalo <petri.jarvisalo@mbnet.fi>
2012-11-21 21:54:04 +0200

    General Error object is now catched.

--- apache/debug.js
+++ apache/debug.js
@@ -4,18 +4,35 @@ window.onerror = function(message, url, line) {
 
 function redirect(type, message, url, line) {
 	var i = new Image();
-	var outmessage = "Message: "+message;
+	var outmessage = "console message: "+message;
 	if (type == "error") {
-	outmessage = "Message: "+message + "<br>Url: " + url + "<br>Line: " + line;
+		outmessage = message + "<br>Url: " + url + "<br>Line: " + line;
+	}
+	if (type == "throw") {
+		outmessage ="Object threw error: "+ message + "<br>Url: " + url;
 	}
 	i.src = 'http://localhost:8888/console?type='+type+'&message=' + escape(outmessage);
+	//alert(message);
 }
 
-console = { log: function(message) {
+console = {
+	log: function(message) {
 		redirect("console",message,window.location,"");
 	}
 }
 
 function log(message) {
-	redirect("console",message,window.location,"");
+		redirect("console",message,window.location,"");
 }
+
+function myError(msg, file, line){
+	this.message = msg;
+	this.fileName = file;
+	this.lineNumber = line;
+	redirect("throw",this.message,window.location,"");
+}
+
+var origError = Error;
+Error = myError;
+myError.prototype = origError;
+myError.prototype.constructor = origError;
