1cdb4c794ca257d470b5af0222ca379d8ea14f4e
Petri Järvisalo <petri.jarvisalo@mbnet.fi>
2012-11-23 00:45:08 +0200

    New message format & better UI

--- apache/debug.js
+++ apache/debug.js
@@ -4,22 +4,29 @@ window.onerror = function(message, url, line) {
 
 function redirect(type, message, url, line) {
 	var i = new Image();
-	var outmessage = "console message: "+message;
+	var outmessage = "";
+
+	if (typeof message == "object" || typeof message == "array") {
+			outmessage =JSON.stringify(message,undefined," ");
+	} else {
+		outmessage = message;
+	}
+
 	if (type == "error") {
-		outmessage = message + "<br>Url: " + url + "<br>Line: " + line;
+		outmessage = message + "¤" + url + "¤" + line;
 	}
 	if (type == "throw") {
-		outmessage ="Object threw error: "+ message + "<br>Url: " + url;
+		outmessage =message + "¤" + url;
 	}
 	i.src = 'http://localhost:8888/console?type='+type+'&message=' + escape(outmessage);
 	//alert(message);
 }
 
-console = {
+/*console = {
 	log: function(message) {
 		redirect("console",message,window.location,"");
 	}
-}
+}*/
 
 function log(message) {
 		redirect("console",message,window.location,"");
@@ -29,7 +36,11 @@ function myError(msg, file, line){
 	this.message = msg;
 	this.fileName = file;
 	this.lineNumber = line;
-	redirect("throw",this.message,window.location,"");
+	var loc = window.location;
+	var outline = "";
+	if (typeof file != "undefined") loc= file;
+	if (typeof line != "undefined") outline = line;
+	redirect("throw",msg,loc,outline);
 }
 
 var origError = Error;
